73d2f442ea1003129449f5196dbab43336f79e02
Siarhei Rybakou <srybak@ext.uber.com>
2021-09-06 20:18:38 +0200

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import classes from './Header.module.css';
 
 const Header = memo(() => {
-  return <span className={classes.RoboFriends}>robofriends</span>;
+  return <span className={classes.RoboFriends}>robofriends app</span>;
 });
 
 export default Header;
